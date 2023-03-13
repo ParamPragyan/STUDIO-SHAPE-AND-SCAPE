6b957f4feae4251cbae7231ec0281ec6dae9686f
Param Pragyan <parampragyan@gmail.com>
2023-03-13 11:56:09 +0530

    optimizing the code

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -8,14 +8,14 @@ const NavBar = () => {
    <nav className="navbar">
     <h3 className="logo">Logo</h3>
 
-    <ul className="nav-links">
-        <NavLink to = '/'><li>Home</li></NavLink>
-        <NavLink to = '/about'><li>About</li></NavLink>
-        <NavLink to = '/services'><li>Services</li></NavLink>
-        <NavLink to = '/work'><li>Works</li></NavLink>
-        <NavLink to = '/contact'><li>Contact</li></NavLink>
-
-    </ul>
+    <div className="nav-links">
+        <NavLink to = '/' className="link">Home</NavLink>
+        <NavLink to = '/about' className="link">About</NavLink>
+        <NavLink to = '/services' className="link">Services</NavLink>
+        <NavLink to = '/work' className="link">Works</NavLink>
+        <NavLink to = '/contact' className="link">Contact</NavLink>
+ 
+    </div>
    </nav>
   )
 }
