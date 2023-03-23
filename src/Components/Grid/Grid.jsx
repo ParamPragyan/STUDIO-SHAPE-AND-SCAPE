38a5bb515ef817982fe72508b06733e089f5fc4f
Pritamdhar_official <pritamdhar6568@gmail.com>
2023-03-23 19:25:43 +0530

    minor changes

--- src/Components/Grid/Grid.jsx
+++ src/Components/Grid/Grid.jsx
@@ -1,25 +1,51 @@
 import React from "react";
-import Data from "../Grid.json";
+// import Data from "../Grid.json";
 import "./Grid.css"
 
-export default function Grid() {
+ function Grid() {
   return (
-    <section className="bg-black ">
+    // <section className="bg-black ">
     <div className="p-10 pb-[10rem] pt-[10rem] bg-white-800 text-black-200">
       <div className="max-w-5xl mx-auto">
         <div className="grid grid-column-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8  ">
-          {
+          {/* {
                   Data.map((data) =>{
                     return (
-                      <div className=' box1 shadow-xl shadow-gray-400 hover:-translate-y-6 duration-150' >
-                        <img src={data.path} key={data.id} alt="" className="w-full h-64 object-cover rounded "/>
+                      <div className=' box1 shadow-xl shadow-gray-400 hover:-translate-y-6 duration-150 ' >
+                        <img src={data.path}  alt="" className="w-full h-64 object-cover rounded "/>
                       </div>
                     )
                   })
-                }
+                } */}
+                <div className="shadow-xl shadow-gray-400 hover:-translate-y-6 duration-150">
+            <img src="images/aj5.jpg" alt="" srcset="" className="rounded" />
+          </div>
+          <div className="shadow-xl shadow-gray-400 hover:-translate-y-6 duration-150">
+            <img src="images/aj5.jpg" alt="" srcset="" className="rounded" />
+          </div>
+          <div className="shadow-xl shadow-gray-400 hover:-translate-y-6 duration-150">
+            <img src="images/aj5.jpg" alt="" srcset="" className="rounded" />
+          </div>
+          <div className="shadow-xl shadow-gray-400 hover:-translate-y-6 duration-150">
+            <img src="images/aj5.jpg" alt="" srcset="" className="rounded" />
+          </div>
+          <div className="shadow-xl shadow-gray-400 hover:-translate-y-6 duration-150">
+            <img src="images/aj5.jpg" alt="" srcset="" className="rounded" />
+          </div>
+          <div className="shadow-xl shadow-gray-400 hover:-translate-y-6 duration-150">
+            <img src="images/aj5.jpg" alt="" srcset="" className="rounded" />
+          </div>
+          <div className="shadow-xl shadow-gray-400 hover:-translate-y-6 duration-150">
+            <img src="images/aj5.jpg" alt="" srcset="" className="rounded" />
+          </div>
+          <div className="shadow-xl shadow-gray-400 hover:-translate-y-6 duration-150">
+            <img src="images/aj5.jpg" alt="" srcset="" className="rounded" />
+          </div> 
                  </div>
       </div>
     </div>
-    </section>
+    // </section>
+      
   );
 }
+export default Grid
