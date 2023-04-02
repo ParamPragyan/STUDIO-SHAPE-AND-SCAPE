ca3189bc4e1778065366e246d689b9d32bf95150
Pritamdhar_official <pritamdhar6568@gmail.com>
2023-04-03 03:15:30 +0530

    added scroll to  number increment feature

--- src/Components/Home/ChooseUs/ChooseUs.jsx
+++ src/Components/Home/ChooseUs/ChooseUs.jsx
@@ -3,8 +3,8 @@ import "./ChooseUs.css";
 
 export default function ChooseUs() {
   return (
-    <section className="py-[5rem] h-auto bg-[#080808] mb-[10rem]  text-gray-100">
-      <div className="pb-[4rem]"><h1 className="flex  justify-center tag text-[3rem] font-[500] font-[Oswald] ">WHY CHOOSE US</h1></div>
+    <section className=" pb-[5rem] h-auto bg-[#080808] mb-[10rem]  text-gray-100">
+      <div className="pb-[4rem] flex "><h1 className="ml-[8rem] px-[1rem] text-black  text-[2rem] bg-white  font-[Oswald] ">WHY CHOOSE US</h1></div>
 
       <div className="flex justify-center gap-0">
         <div className="flex flex-col  p-5 gap-10  w-[24rem]  ">
--- src/Components/Slider-second/Stats/Stats.jsx
+++ src/Components/Slider-second/Stats/Stats.jsx
@@ -1,31 +1,41 @@
 import React from "react";
+import { useState } from "react";
 import "./Stats.css";
+import CountUp from "react-countup";
+import ScrollTrigger from "react-scroll-trigger";
 
 const Stats = () => {
+  const [counterOn, setCounterOn]=useState(false);
   return (
     <>
+    <ScrollTrigger onEnter={()=>setCounterOn(true)} onExit={()=>setCounterOn(false)}>
       <section className="flex justify-around items-center p-20 bg-black text-white stats">
-      <div className=" flex flex-col items-center total-projects">
-        <h1>99+</h1>
-        <p>Total Projects</p>
-      </div>
-      <div className="flex flex-col items-center ongoing-projects">
-        <h1>99+</h1>
-        <p>Ongoing Projects</p>
-      </div>
-      <div className="flex flex-col items-center completed-projects">
-        <h1>99+</h1>
-        <p>Completed Projects</p>
-      </div>
-
-    </section>
-    {/* <div className="bg-black">
+     
+        <div className=" flex flex-col items-center total-projects">
+          <h1>
+           {counterOn && <CountUp start={0} end={99} duration={5} delay={0} />}+
+          </h1>
+          <p>Total Projects</p>
+        </div>
+        <div className="flex flex-col items-center ongoing-projects">
+          <h1>
+          {counterOn && <CountUp start={0} end={99} duration={5} delay={0} />}+
+          </h1>
+          <p>Ongoing Projects</p>
+        </div>
+        <div className="flex flex-col items-center completed-projects">
+          <h1>
+          {counterOn && <CountUp start={0} end={99} duration={5} delay={0} />}+
+          </h1>
+          <p>Completed Projects</p>
+        </div>
+        
+      </section>
+      </ScrollTrigger>
+      {/* <div className="bg-black">
     <div className=' w-5/12 m-auto h-px bg-gray-600'></div>
     </div> */}
-   
-
     </>
-  
   );
 };
 
